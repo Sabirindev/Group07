0c38d6410be46b6728ab22f142a5a9fd9eb65797
sabir mustafayev <sdetinscrum@gmail.com>
2021-08-04 11:04:52 -0400

    Created Variables.java file under day01 folder

---
+++ src/day01/Variables.java
@@ -0,0 +1,20 @@
+package day01;
+
+public class Variables {
+    public static void main(String[] args) {
+        //Primitive DataTypes - whole numbers
+        byte b1 = 10;
+        short s1 = 15;
+        int  a1 = 20;
+        long l1 = 25L;
+
+        //Primitive DataTypes - float numbers
+        float f1 = 1.5f;
+        double d1 = 2.5;
+
+        char ch1 = 65; // 'A'
+        boolean r1 = false; // true
+
+        System.out.println(ch1); // A
+    }
+}
