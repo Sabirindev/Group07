32d53366b706861da5b4dfe637e0b5bba7a8c2e4
blishchenko1981 <blishchenko1981@gmail.com>
2021-08-05 16:49:20 -0400

    Vitalii car

--- src/beginning/Car.java
+++ src/beginning/Car.java
@@ -2,4 +2,7 @@ package beginning;
 
 public class Car {
     //Please each of us add one car brand in println method
+    public static void main(String[] args) {
+        System.out.println("Honda");
+    }
 }
