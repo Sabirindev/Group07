8bec2557bfd5026f9f1430a629c6bb1f5abc8cba
Marina Vel <marinavelitskaia@gmail.com>
2021-08-05 15:25:47 -0400

    commit1

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("day1");
+        //i dont see this file on git hub
     }
 }
