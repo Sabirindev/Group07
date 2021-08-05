b1fbb541efb3daf286aecf3fc2401b8ac86f809f
sabir mustafayev <sdetinscrum@gmail.com>
2021-08-05 17:21:31 -0400

    added "Lexus" into Car class

--- src/beginning/Car.java
+++ src/beginning/Car.java
@@ -2,4 +2,9 @@ package beginning;
 
 public class Car {
     //Please each of us add one car brand in println method
+    public static void main(String[] args) {
+        String carBrand = "Jaguar";
+        System.out.println("Honda");
+        System.out.println("Lexus");
+    }
 }
