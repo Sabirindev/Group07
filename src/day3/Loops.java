68c7d95a440dc70481eaa1afa8c6f0ccdd38714b
atila-a <abulajianga@gmail.com>
2021-08-05 16:20:03 -0700

    atila added line 6

--- src/day3/Loops.java
+++ src/day3/Loops.java
@@ -3,7 +3,7 @@ package day3;
 public class Loops {
 
     // changes from Sabir
-
+// changes from Atila
     public static void main(String[] args) {
         System.out.println("Loops");
         System.out.println("Loops");
