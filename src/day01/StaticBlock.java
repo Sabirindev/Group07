4953b07a0693109eaa093f103e45ad99c2a90578
abbos97 <abbosboev@gmail.com>
2021-08-05 14:14:16 -0400

    Practice with static block

---
+++ src/day01/StaticBlock.java
@@ -0,0 +1,19 @@
+package day01;
+
+public class StaticBlock {
+
+    public static String name;
+    public static int number;
+    public static char aChar;
+    public static double num;
+
+    static {
+        name="CybertekSchool";
+    }
+    static {
+        number=10;
+    }
+    static {
+        aChar='A';
+    }
+}
