a0e036d016a57849519306d56973e6963d11bfba
sabir mustafayev <sdetinscrum@gmail.com>
2021-08-05 15:36:43 -0400

    created FriendlyReminding class for reminding for discussion on GitHub

---
+++ src/day01/FriendlyReminding.java
@@ -0,0 +1,6 @@
+package day01;
+
+public class FriendlyReminding {
+    //Please before merging your changes (your branch) to master branch on GitHup,
+    //try to create pull request..then we all can make discussion about your changes
+}
