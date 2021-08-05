6343f78eef81c26c9104eeea215c8d8f1b9310e4
AbbosB12 <58409711+AbbosB12@users.noreply.github.com>
2021-08-05 19:15:34 -0400

    Revert "changes from Sabir"

--- src/day3/Loops.java
+++ src/day3/Loops.java
@@ -1,9 +1,6 @@
 package day3;
 
 public class Loops {
-
-    // changes from Sabir
-
     public static void main(String[] args) {
         System.out.println("Loops");
         System.out.println("Loops");
