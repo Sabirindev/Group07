701c63c58e6d27e0d7edc3b7e90b3aa54ad9b23f
SHOKHZODJURAEV <shahaking9393@gmail.com>
2021-08-05 19:48:14 -0400

    new with friend

--- src/day3/Loops.java
+++ src/day3/Loops.java
@@ -5,9 +5,12 @@ public class Loops {
     // changes from Sabir
 // changes from Atila
     // changes from Vitalii
+
     public static void main(String[] args) {
         System.out.println("Loops");
         System.out.println("Loops");
+
+        // new with friend
 
 
 
@@ -17,5 +20,4 @@ public class Loops {
     }
 
 
-}
 
