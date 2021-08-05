2d1a472706bce10a96b0a5fdd63644a68d21ca32
blishchenko1981 <blishchenko1981@gmail.com>
2021-08-05 19:28:06 -0400

    Vitalii car

--- src/day3/Loops.java
+++ src/day3/Loops.java
@@ -4,9 +4,12 @@ public class Loops {
 
     // changes from Sabir
 // changes from Atila
+    // changes from Vitalii
     public static void main(String[] args) {
         System.out.println("Loops");
         System.out.println("Loops");
+
+
     }
 
 
