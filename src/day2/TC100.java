efbe1a3d93f8016edc2fde6bfa9c34f5a8ae77e5
abbos97 <abbosboev@gmail.com>
2021-08-05 16:35:58 -0400

    Create static block

--- src/day2/TC100.java
+++ src/day2/TC100.java
@@ -1,5 +1,13 @@
 package day2;
 
 public class TC100 {
+    public static String name;
+    public static int num;
+    public static double number;
 
+    static {
+        name="Apple";
+        num=12;
+        number=12.5;
+    }
 }
