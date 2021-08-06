cc6fb03d9f01cf5660002036004fd20bfe8ed028
abbos97 <abbosboev@gmail.com>
2021-08-06 10:52:37 -0400

    My code is completed.

--- src/day4/Login.java
+++ src/day4/Login.java
@@ -3,4 +3,12 @@ package day4;
 public class Login {
 // worked 1 is done
     //worked commpeleted
+
+    public static String name;
+    static {
+        name="CybertekSchool";
+    }
+public static void main(String[] args) {
+
+}
 }
