1b7d1000d34b1c92530f2adfe3ab05c577868fee
Mansurov1990 <mansurov1990@gmail.com>
2021-08-07 00:16:24 -0400

    Farid created new Package, created first Java class.

---
+++ src/Farid_Mansurov/Farid1.java
@@ -0,0 +1,11 @@
+package Farid_Mansurov;
+
+public class Farid1 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Everyone!");
+        System.out.println("Farid joined project");
+
+
+    }
+}
