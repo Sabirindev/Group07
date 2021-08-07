b748e0e71c4081f08cb1f8fd438b1d012cb1369d
KseniiaMazanko <ksmazanko@gmail.com>
2021-08-06 20:51:09 -0400

    ArrayList added

--- src/TestCaseKseniia/TestCase1.java
+++ src/TestCaseKseniia/TestCase1.java
@@ -1,8 +1,12 @@
 package TestCaseKseniia;
 
+import java.util.ArrayList;
+
 public class TestCase1 {
 
     public static void main(String[] args) {
+
+        ArrayList<String>group7 = new ArrayList<>();
 
     }
 
