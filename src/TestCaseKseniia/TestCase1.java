98b2d99e111c7a96a68db22db8458eac1b8fb092
KseniiaMazanko <87542358+KseniiaMazanko@users.noreply.github.com>
2021-08-06 21:04:03 -0400

    Kseniia's test case (#44)
    * main method added
    
    * print out added
    
    * third commit added
    
    * test case is done
    
    * added a new car brand
    
    * printed BMW
    
    * new car added and printed
    
    * main method added
    
    * ArrayList added
    
    * The fact added
    
    * Test case is done

---
+++ src/TestCaseKseniia/TestCase1.java
@@ -0,0 +1,18 @@
+package TestCaseKseniia;
+
+import java.util.ArrayList;
+
+public class TestCase1 {
+
+    public static void main(String[] args) {
+
+        ArrayList<String>group7 = new ArrayList<>();
+
+        System.out.println("Group 7 is the best group!");
+
+        //test case is done
+
+    }
+
+
+}
