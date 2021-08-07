ba4914e1b6900b0ba9599ab45d3b644e3749ad95
KseniiaMazanko <ksmazanko@gmail.com>
2021-08-06 20:42:51 -0400

    added a new car brand

--- src/beginning/Car.java
+++ src/beginning/Car.java
@@ -8,12 +8,20 @@ public class Car {
 
         System.out.println("Honda");
         System.out.println("Lexus");
-       String carBrand = "Jaguar";
-        System.out.println(carBrand);
-      System.out.println("Honda");
+        String carBrand2 = "Jaguar";
+        System.out.println(carBrand2);
+        System.out.println("Honda");
         
         System.out.println(carBrand);
-         String carBrand = "Jaguar";
+        String carBrand3 = "Jaguar";
+
+        //adding a new car brand
+        String carBrand4 = "BMW";
+
+
+
+
+
         
     }
 
